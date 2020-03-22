41c197639b5692a40ea1dc334c8374fc14cf51fb
Nelwey <nelwey1234@gmail.com>
2020-03-21 22:52:50 -0400

    segundo commit

--- js/script.js
+++ js/script.js
@@ -12,7 +12,9 @@ $(document).ready(function () {
 
     for (var i = 1; i < 65; i++) {
 
-      $('#tablero').append(`<div class='col-4 col-sm-2 p-1 bg-success text-white border'><small>
+      $('#tablero').append(`<div class='col-4 col-sm-1 p-1 bg-success text-white border'>
+      <small class='border rounded p-1 text-white bg-warning font-weight-bold'>${i}</small><br>
+      <small>
       ${b}
       </small></div>`);
       a = b;
